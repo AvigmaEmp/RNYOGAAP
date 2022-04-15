205d3c326e7823e9e8b0057fb434a20926f7dde9
AvigmaEmp <103031047+AvigmaEmp@users.noreply.github.com>
2022-04-15 15:05:51 +0530

    Add files via upload

---
+++ api.function.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+import { API } from "./api.types";
+import { Dimensions, Platform, PixelRatio } from "react-native";
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+const scale = SCREEN_WIDTH / 320;
+export const register = async (data) => {
+  return axios(API.REGISTRATION_API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+    data: data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const login = async (data) => {
+  return axios(API.LOGIN_API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+    data: data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+export const forgotpassword = async (data) => {
+  return axios(API.FORGOT_PASSWORD, {
+    method: "POST",
+    data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+export const resetpassword = async (data) => {
+  return axios(API.RESET_PASSWORD, {
+    method: "POST",
+    data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+export const registerstoreimage = async (data, access_token) => {
+  return axios(API.STORE_IMAGE_API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + access_token,
+    },
+    data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+export const userprofile = async (data, access_token) => {
+  return axios(API.GET_USER_DATA, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + access_token,
+    },
+    data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+export const userprofileupdate = async (data, access_token) => {
+  return axios(API.UPDTAE_USER_DETAIL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + access_token,
+    },
+    data,
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export function normalize(size) {
+  const newSize = size * scale;
+  if (Platform.OS === "ios") {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  }
+}
---
+++ api.types.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "http://apimusic.ikaart.org";
+
+export const API = {
+  REGISTRATION_API: BASE_URL + "/token",
+  LOGIN_API: BASE_URL + "/token",
+  GET_USER_DATA: BASE_URL + "/api/Music/GetUserMasterData",
+  FORGOT_PASSWORD: BASE_URL + "/api/Music/ForGotPassword",
+  RESET_PASSWORD: BASE_URL + "/api/Music/ChangePasswordByEmail",
+  UPDTAE_USER_DETAIL: BASE_URL + "/api/Music/AddUserMasterData",
+};
